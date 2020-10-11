b52cc8a2d6a9f24b26481f074a84ba9520988460
Idanatiya <idanatiya122@gmail.com>
2020-10-12 02:23:31 +0300

    fixed photos

--- js/services/proj-service.js
+++ js/services/proj-service.js
@@ -3,7 +3,7 @@
 
 var gProjects = [
     { id: 'minesweeper', name: 'Mine Sweeper', title: 'Sprint 1 Project', desc: 'This project was the first Sprint in Coding Academy bootcamp, This Mine Sweeper contains alot of cool features such as Hints,Save button and Undo!', publishedAt: Date.now(), labels: ['HTML', 'CSS', 'JS'] },
-    { id: 'book-shop', name: 'Book Shop', title: 'CRUDL Project ', desc: 'This project was a coding challenge to create a book shop with CRUDL', publishedAt: new Date(2020, 9, 10, 3, 24, 0).toLocaleString(), labels: ['HTML', 'CSS', 'JS'] },
+    { id: 'book-shop', name: 'Book Shop', title: 'CRUDL Project ', desc: 'This project was a coding challenge to create a book shop with CRUDL', publishedAt: Date.now(), labels: ['HTML', 'CSS', 'JS'] },
     { id: 'in-picture', name: 'What is in the picture', title: 'cool project', desc: 'This project was the first time i rendered dynmically in js', publishedAt: Date.now(), labels: ['HTML', 'CSS', 'JS'] },
     { id: 'pacman', name: 'Sonic Pacman', title: 'Pacman project', desc: 'This project was a good project to learn how to manage file ', publishedAt: Date.now(), labels: ['HTML', 'CSS', 'JS'] },
     { id: 'safe-content', name: 'Safe Content', title: 'MVC Login + Admin Page', desc: 'This project was a good project to practice MVC', publishedAt: Date.now(), labels: ['HTML', 'CSS', 'JS'] },
